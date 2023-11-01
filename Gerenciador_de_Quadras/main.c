821db8645fd13e6cbff4f1cde1932772108af44f
Alona <a249664@dac.unicamp.br>
2023-11-01 15:01:20 -0300

    +Reading Prototype +Reading Prototype +.txt format to DateFormat +Dynamic Directory

--- Gerenciador_de_Quadras/main.c
+++ Gerenciador_de_Quadras/main.c
@@ -16,13 +16,15 @@
 #include <string.h>
 
 #define LSIZE 200
-#define DIR "C:\\Users\\a249664\\Documents\\Projeto SI203B\\Data\\"
+#define DIR "..\\Gerenciador_de_Quadras\\Data\\"
+
 #define LBREMOVER(string) string[strlen(string)-1] = '\0';
+#define FAILVERIFY(pointer) if(pointer == NULL){printf("ERROR!!");exit(1);}
 
 void DateTreat(char *Date);
 void DateGet(char *day, char *month, char *year);
 void DateFormat(char *day, char *month, char *year, char *entiredate);
-
+void LowerCase(char *string);
 
 struct QuadraInfo{
 	char name[LSIZE];
@@ -43,11 +45,51 @@ struct Date{
 int main(){
 	
 	FILE *FilePointer;//Ponteiro de Arquivo
-	char Directory[LSIZE] = DIR;
-	
+	char Directory[LSIZE];// = DIR;
+	char *UserCommand = malloc(LSIZE*sizeof(char));
+	char *TextData = malloc(LSIZE*sizeof(char));
 	struct Date *ActualDate;
 	ActualDate = (struct Date*) malloc(sizeof(struct Date));
 
+start:
+	Directory[0] ='\0';
+	strcat(Directory, DIR);
+	
+	printf("\n\n\t\tPick your poison\n\tWriting\t\t\tReading\n\n\t\t   ");
+	fgets(UserCommand, LSIZE, stdin); LBREMOVER(UserCommand)
+	LowerCase(UserCommand);
+	
+/* Protótipo de Escrita */	
+	if(!(strcmp(UserCommand, "writing"))){
+		printf("\nPlease insert date\n\n");
+		DateGet(ActualDate->day, ActualDate->month, ActualDate->year);
+		DateFormat(ActualDate->day,ActualDate->month,ActualDate->year,ActualDate->FullDate);
+		strcat(Directory, ActualDate->FullDate);
+		//printf("%s", Directory);
+		FilePointer = fopen(Directory, "w");
+		printf("Write your data\n\n");
+		fgets(UserCommand, LSIZE, stdin);
+		fwrite(UserCommand, sizeof(char), strlen(UserCommand), FilePointer);
+		fclose(FilePointer);
+		goto start;
+	}
+	
+/* Protótipo de Leitura */
+
+	if(!(strcmp(UserCommand, "reading"))){
+		printf("\nPlease insert date\n\n");
+		DateGet(ActualDate->day, ActualDate->month, ActualDate->year);
+		DateFormat(ActualDate->day,ActualDate->month,ActualDate->year,ActualDate->FullDate);
+		strcat(Directory, ActualDate->FullDate);
+		FilePointer = fopen(Directory, "r");
+		fgets(TextData, LSIZE, FilePointer);
+		printf("\nYour info:\n\n%s\n\n", TextData);
+		fclose(FilePointer);
+		goto start;
+	}
+
+
+/*Protótipo de Aquisição de Data*/
 	DateGet(ActualDate->day, ActualDate->month, ActualDate->year);
 	DateFormat(ActualDate->day,ActualDate->month,ActualDate->year,ActualDate->FullDate);
 	printf("%s\n", ActualDate->FullDate);
@@ -98,6 +140,20 @@ void DateFormat(char *day, char *month, char *year, char *entiredate){
     
 	entiredate[0] = day[0];entiredate[1] = day[1];entiredate[2] = '.';
 	entiredate[3] = month[0];entiredate[4] = month[1];entiredate[5] = '.';
-	entiredate[6] = year[0];entiredate[7] = year[1];entiredate[8] = year[2];entiredate[9] = year[3];entiredate[10] = '\0';
+	entiredate[6] = year[0];entiredate[7] = year[1];entiredate[8] = year[2];entiredate[9] = year[3];
+	entiredate[10] = '.'; entiredate[11] = 't'; entiredate[12] = 'x'; entiredate[13] = 't'; entiredate[14] = '\0';
+}
+
+void LowerCase(char *string){
+
+	int index;
+	
+	for(index = 0; index<strlen(string); index++)
+	{
+		if(string[index]<97)
+		{
+			string[index] +=32;
+		}
+	}	
 }
 
