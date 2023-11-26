e32fb13d1d2c1abd650530a6727ff01ba07817ae
Alona Nicolau de Oliveira <135407629+Alonum@users.noreply.github.com>
2023-11-25 23:23:19 -0300

    Update main.c
    Adicionado todas as funções, um dia olho isso to cansada foda-se

--- Gerenciador_de_Quadras/main.c
+++ Gerenciador_de_Quadras/main.c
@@ -12,7 +12,7 @@
 
 FEITO				*Adicionar Quadra Volei, Quadra Basquete, Quadra Futebol
 A FAZER				*Adicionar Preco 200 Reais Fixo
-QUASE FEITO			*Finalizar Protótipo de Verificacao	
+QUASE FEITO			*Finalizar ProtÃ³tipo de Verificacao	
 */
 
 #include <stdio.h>
@@ -21,9 +21,15 @@ QUASE FEITO			*Finalizar Protótipo de Verificacao
 
 #define LSIZE 200
 #define DIR "..\\Gerenciador_de_Quadras\\Data\\"
+#define DAYDIR "..\\Gerenciador_de_Quadras\\Data\\days.txt"
 
 #define LBREMOVER(string) string[strlen(string)-1] = '\0';
 #define FAILVERIFY(pointer) if(pointer == NULL){printf("ERROR!!");exit(1);}
+
+#define WINDOWS system("cls");
+#define LINUX system("clear");
+
+#define CLEARSCREEN WINDOWS
 
 void SingleNumberTreat(char *Number);
 void DateGet(char *day, char *month, char *year);
@@ -51,20 +57,35 @@ struct Date{
 int main(){
 	
 	//wip
-	int Index, IndexCompare, IndexPosition, IndexNegative;
+	int Index, IndexTime, IndexCompare, IndexPosition, IndexNegative;
 	//wip
 	
-	FILE *FilePointer;//Ponteiro de Arquivo
+
 	char Directory[LSIZE];// = DIR;
-	char *InfoCompare = malloc(10*sizeof(char));
-	char *UserCommand = malloc(LSIZE*sizeof(char));
-	char *TextData = malloc(LSIZE*sizeof(char));
+
+	FILE *FilePointer;//Ponteiro de Arquivo
+	char *InfoCompare;// = malloc(10*sizeof(char));
+	char *UserCommand;// = malloc(LSIZE*sizeof(char));
+	char *TextData;// = malloc(LSIZE*sizeof(char));
+
+	char *QuadraCompare;// = malloc(LSIZE*sizeof(char));
+	char *TimeCompare;// = malloc(LSIZE*sizeof(char));
+
 	struct Date *ActualDate;
 	struct QuadraInfo *InfoAtual;
+//	ActualDate = (struct Date*) malloc(sizeof(struct Date));
+//	InfoAtual = (struct QuadraInfo*) malloc(sizeof(struct QuadraInfo));
+
+start:
+	
+	InfoCompare = malloc(10*sizeof(char));
+	UserCommand = malloc(LSIZE*sizeof(char));
+	TextData = malloc(LSIZE*sizeof(char));
+	QuadraCompare = malloc(LSIZE*sizeof(char));
+	TimeCompare = malloc(LSIZE*sizeof(char));
 	ActualDate = (struct Date*) malloc(sizeof(struct Date));
 	InfoAtual = (struct QuadraInfo*) malloc(sizeof(struct QuadraInfo));
 
-start:
 	Directory[0] ='\0';
 	strcpy(Directory, DIR); //Reinicia diretorio
 	strcpy(TextData, "Emtpy\0"); //Reinicia info de output dos arquivos
@@ -78,20 +99,67 @@ start:
 	if(!(strcmp(UserCommand, "marcar"))){
 		printf("\nData do agendamento:\n\n");
 		
+
+
 		/*Adquirir nome do arquivo do dia desejado*/
 		DateGet(ActualDate->day, ActualDate->month, ActualDate->year);
 		DateFormat(ActualDate->day,ActualDate->month,ActualDate->year,ActualDate->FullDate);
 		strcat(Directory, ActualDate->FullDate);
+
+
+		/*Adicionar dia do agendamento em days .txt*/
+		//Verifica primeiro se ha algum agendamento
+
+		FilePointer = fopen(DAYDIR, "r");
+		IndexPosition = 0;
+		fseek(FilePointer,0, SEEK_SET);
+		while(fgetc(FilePointer)!= EOF){
+			for(Index=0;fgetc(FilePointer)!= '>';Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				TextData[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			IndexPosition += 3;
+			TextData[Index] = '\0';
+			if(!strcmp(ActualDate->FullDate, TextData)){
+				goto next;
+			}
+		
+		fgetc(FilePointer);
+		}
+		//marca no arquivo, caso nao ache oposicao
+		fclose(FilePointer);
+		strcpy(TextData, ActualDate->FullDate);
+		TextData[Index]= '>';
+		TextData[Index+1]= '\n';
+		TextData[Index+2]= '\0';
+		FilePointer= fopen(DAYDIR, "a");
+		fwrite(TextData, sizeof(char), strlen(TextData), FilePointer);
+		fclose(FilePointer);
+next:
+		fclose(FilePointer);
+
+		/**/    
+
+
+
 		/*Verificar informacoes do agendamento*/
 		printf("Insira as informacoes do Agendamento\n\n");
 		InfoGet(InfoAtual->hour, InfoAtual->minutes, InfoAtual->name, InfoAtual->cpf, InfoAtual->type);
         InfoFormat(InfoAtual->hour, InfoAtual->minutes, InfoAtual->name, InfoAtual->cpf, InfoAtual->type,InfoAtual->fullcase);
-        /*Verificar o arquivo              WIP TOTAL */
+		
+
+
+		strcpy(Directory, DIR);
+		strcat(Directory, ActualDate->FullDate);
+   
+		 /*Verificar o arquivo              WIP TOTAL */
         FilePointer = fopen(Directory, "r");
         IndexPosition = 0;
-        IndexNegative = 0;
+		fseek(FilePointer,0, SEEK_SET);
         while(fgetc(FilePointer)!= EOF){
-			printf("\nComecando em %d\n", IndexPosition);
+			IndexNegative = 0;//contradiz a soma de hora superior para verificar horarios anteriores ao atual
+																	//printf("\nComecando em %d\n", IndexPosition);//debug
 			//Procura Hora
 			for(Index=0;fgetc(FilePointer) != ':'; Index++){
 				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
@@ -104,68 +172,256 @@ start:
             InfoCompare[2] = '\0';
             //Adiciona hora a IndexCompare
             IndexCompare = atoi(InfoCompare);
-            printf("\nHora Atual:%s\n", InfoCompare);
+        												  //  printf("\nHora Atual:%s\n", InfoCompare);//debug
             //Procura Minutos
            	for(Index=0;fgetc(FilePointer) != ';'; Index++){
 				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
 				InfoCompare[Index] = fgetc(FilePointer);
 				IndexPosition++;
 			}
-			
+			//Apos este for, o proximo caractere vem depois de ;, ou seja, a quadra que esta logo em seguida
 			InfoCompare[2] = '\0';
 			
-			for(;fgetc(FilePointer)!= '>';){
-				IndexPosition++;
-				//printf("\nAchamos a quebra de linha\n");
-				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
-			}
-			IndexPosition+=3;
 			
-			//Compara minutos, se houver qualquer minuto adicional, conta a hora.
+
+			//Compara as horas entre si, e caso o horario inserido seja menor que o marcado, e tenha minutos, insere uma hora a mais.
+			IndexTime=atoi(InfoAtual->hour);
+			if(atoi(InfoAtual->minutes)>0 && atoi(InfoAtual->hour)<IndexCompare){
+				IndexTime++;
+			}
+			//Compara minutos, se houver qualquer minuto adicional, conta a hora. Incrementa IndexNegative para contrariar a condicao no caso de horarios anteriores.
 			if(atoi(InfoCompare)>1){
 				IndexCompare++;
 				IndexNegative++;
 			}
-			if((IndexCompare-IndexNegative)-atoi(InfoAtual->hour)<=1 && IndexCompare-atoi(InfoAtual->hour) >=-1){
-        		printf("Hora ja marcada, pois %d menos %d, resulta em %d", IndexCompare, atoi(InfoAtual->hour), IndexCompare-atoi(InfoAtual->hour));
-        		
+
+			
+														//printf("O caractere atual eh:\t%c\n", fgetc(FilePointer));//debug
+			//if de caso a hora seja menos q duas entre cada
+			IndexPosition++;//Sai de ;
+			if((IndexCompare-IndexNegative)-IndexTime<=1 && IndexCompare-IndexTime >=-1){
+				for(Index=0;fgetc(FilePointer)!= ';';Index++){
+					fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+					TextData[Index] = fgetc(FilePointer);
+					IndexPosition++;
+				}
+				TextData[Index]='\0';
+				if(!strcmp(TextData, InfoAtual->type)){
+					printf("\n\nQuadra Reservada no momento\n\n");
+					goto start;
+				}
 			}
-			else
-       			printf("Hora nao marcada, pois %d menos %d, resulta em %d", IndexCompare, atoi(InfoAtual->hour), IndexCompare-atoi(InfoAtual->hour));
-		
+												//	printf("Hora ja marcada, pois %d menos %d, resulta em %d\n A quadra atual eh: %s\n", IndexCompare, IndexTime, IndexCompare-IndexTime,TextData);
+												//printf("Hora nao marcada, pois %d menos %d, resulta em %d", IndexCompare, Index, IndexCompare-Index);
+			//Procura a proxima linha
+			for(;fgetc(FilePointer)!= '\n';){
+				IndexPosition++;
+																										//printf("\nAchamos a quebra de linha\n");
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+			}
+			IndexPosition++;
+
+	
         }
-        printf("Fim do programa");
+        printf("\n\nQuadra Disponivel!\n\n");
+		strcpy(TextData, "Emtpy\0"); //Reinicia info de output dos arquivos
         fclose(FilePointer);
-        return 0;
+
+
+
         /*Escrever em arquivo*/
         FilePointer = fopen(Directory, "a");
 		fwrite(InfoAtual->fullcase, sizeof(char), strlen(InfoAtual->fullcase), FilePointer);
 		fclose(FilePointer);
-		free(InfoAtual);
-		free(ActualDate);
+				free(QuadraCompare); 
+				free(TimeCompare); 
+				free(InfoAtual);
+				free(ActualDate);
+				free(TextData);
+				free(InfoCompare);
+				free(UserCommand);
+		printf("\nPressione enter para continuar\n");
+		while(getchar() != '\n'){getchar();}
+		CLEARSCREEN
 		goto start;
 	}
 /* Prot?ipo de Leitura */
 	else if(!(strcmp(UserCommand, "consultar"))){
-		printf("\nInsira a data do Agendamento\n\n");
+
+		printf("\nEscolha um dia de agendamento:\n\n");
+		//lista os dias disponiveis para consulta
+		FilePointer = fopen(DAYDIR, "r");
+		IndexPosition = 0;
+		fseek(FilePointer,0, SEEK_SET);
+		while(fgetc(FilePointer)!= EOF){
+			for(Index=0;fgetc(FilePointer)!= '\n';Index++){
+					fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+					TextData[Index] = fgetc(FilePointer);
+					IndexPosition++;
+				//printf("\ntiming: %d %s\n",IndexTime,  TextData);
+			}
+			TextData[10]= '\0';
+			TextData[2]= '/';
+			TextData[5]= '/';
+			if(strcmp(TextData, "00/00/0000"))
+			printf("%s\n", TextData);
+			IndexPosition +=2;
+		//	if(!strcmp(ActualDate->FullDate, TextData)){
+		//		goto next;
+		//	}
+		fgetc(FilePointer);
+		}
+		fclose(FilePointer);
+		strcpy(TextData, "Emtpy\0"); //Reinicia info de output dos arquivos
+		//adquire o dia desejado
+		printf("\n");
 		DateGet(ActualDate->day, ActualDate->month, ActualDate->year);
 		DateFormat(ActualDate->day,ActualDate->month,ActualDate->year,ActualDate->FullDate);
 		strcat(Directory, ActualDate->FullDate);
 		FilePointer = fopen(Directory, "r");
-		
-		//WIP
-		fgets(TextData, 3, FilePointer);
-		printf("Hora:%s", TextData);
-		for(Index = 0;fgetc(FilePointer) != ';';Index++){
-			fseek(FilePointer, sizeof(char), SEEK_CUR);
-		//	fgetc(FilePointer);
-			TextData[Index] = fgetc(FilePointer);
-			printf("\nEstado %d: %s\n", Index, TextData);
+		if(FilePointer == NULL){
+			printf("\n!!Data Invalida!!\n");
+			goto start;
 		}
-		//fgets(TextData, 10, FilePointer);
-		printf("Nome: %s", TextData);
+		//WIP
+		printf("\nEscolha o horario e quadra que deseja consultar\n\n");
+		IndexPosition = 0;
+		fseek(FilePointer,0, SEEK_SET);
+		while(fgetc(FilePointer) != EOF){
+			for(Index=0;fgetc(FilePointer) != ';'; Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				InfoCompare[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			IndexPosition++;
+			InfoCompare[Index] = '\0';
+			printf("Horario: %s\t\t--\t", InfoCompare);
+			for(Index=0;fgetc(FilePointer) != ';'; Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				InfoCompare[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			InfoCompare[Index] = '\0';
+			printf("Quadra de %s\n", InfoCompare);
+
+			for(;fgetc(FilePointer)!='\n';){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				IndexPosition++;
+			}
+
+			IndexPosition++;
+			fgetc(FilePointer);
+		}
+		//Escolhe a hora que deseja utilizar
+	choose:
+		printf("Insira a hora:\t");
+        fgets(TextData,10,stdin); LBREMOVER(TextData)   
+        if(atoi(TextData)>22 || atoi(TextData)<0){
+            printf("Hora invalida, pressione enter para inserir os dados novamente\n");
+            while(getchar() != '\n'){getchar();}
+            goto choose;
+        }
+        printf("Insira os minutos:\t");
+        fgets(InfoCompare,10,stdin); LBREMOVER(InfoCompare)
+        if(atoi(InfoCompare)>59 || atoi(InfoCompare)<0 ||(atoi(TextData)==22 && atoi(InfoCompare)>0)){
+            printf("Minuto invalido, pressione enter para inserir os dados novamente\n");
+            while(getchar() != '\n'){getchar();}
+            goto choose;
+        }
+		//trata a hora para ficar igual a string
+		SingleNumberTreat(TextData);
+		SingleNumberTreat(InfoCompare);
+		TextData[2] = ':';
+		TextData[3] = '\0';
+		strcat(TextData, InfoCompare);
+		//escolhe a quadra que deseja utilizar
+
+	choosequadra:
+		printf("Insira a quadra:\t");
+		fgets(InfoCompare,20,stdin); LBREMOVER(InfoCompare)
+		LowerCase(InfoCompare);
+		if(!(!strcmp(InfoCompare,"volei") || !strcmp(InfoCompare ,"futebol") || !strcmp(InfoCompare, "basquete"))){
+			printf("Quadra invalida, pressione enter para inserir os dados novamente\n");
+			while(getchar() != '\n'){getchar();}
+			goto choosequadra;
+		}
 		
-		printf("\nYour info:\n\n%s\n\n", TextData);
+		//compara o horario e quadra escolhida com os horarios e quadras de dentro
+		strcpy(QuadraCompare, InfoCompare);
+		strcpy(TimeCompare, TextData);
+		fseek(FilePointer,0, SEEK_SET);
+		IndexPosition = 0;
+		
+
+		while(fgetc(FilePointer) != EOF){
+			for(Index=0;fgetc(FilePointer) != ';'; Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				InfoCompare[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			IndexPosition++;
+			InfoCompare[Index] = '\0';
+			if(!strcmp(InfoCompare, TimeCompare)){
+				for(Index=0;fgetc(FilePointer) != ';'; Index++){
+					fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+					InfoCompare[Index] = fgetc(FilePointer);
+					IndexPosition++;
+				}
+				InfoCompare[Index] = '\0';
+				if(!strcmp(InfoCompare, QuadraCompare)){
+					printf("\n\n\nInfo da data:\n\nHorario: %s\tQuadra: %s\n",TimeCompare, QuadraCompare);
+					IndexPosition++;
+					for(Index=0;fgetc(FilePointer) != ';'; Index++){
+						fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+						InfoCompare[Index] = fgetc(FilePointer);
+						IndexPosition++;
+					}
+					IndexPosition++;
+					InfoCompare[Index] = '\0';
+					printf("Locatario: %s\t\t", InfoCompare);
+
+					for(Index=0;fgetc(FilePointer) != '>'; Index++){
+						fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+						InfoCompare[Index] = fgetc(FilePointer);
+						IndexPosition++;
+					}
+					InfoCompare[Index] = '\0';
+					printf("CPF: %s\nValor: R$200.00\n\n", InfoCompare);
+
+					printf("\nPressione enter para continuar\n");
+					while(getchar() != '\n'){getchar();}
+					CLEARSCREEN
+				free(QuadraCompare); 
+				free(TimeCompare); 
+				free(InfoAtual);
+				free(ActualDate);
+				free(TextData);
+				free(InfoCompare);
+				free(UserCommand);
+					fclose(FilePointer);
+					goto start;
+				}
+			}
+
+			for(;fgetc(FilePointer)!='\n';){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				IndexPosition++;
+			}
+
+			IndexPosition++;
+			fgetc(FilePointer);
+		}
+		printf("\n\nNenhum agendamento com estes dados encontrados");
+		printf("\n\nPressione enter para continuar\n");
+		while(getchar() != '\n'){getchar();}
+		CLEARSCREEN
+		free(QuadraCompare); 
+		free(TimeCompare); 
+		free(InfoAtual);
+		free(ActualDate);
+		free(TextData);
+		free(InfoCompare);
+		free(UserCommand);
 		fclose(FilePointer);
 		goto start;
 		//WIP
@@ -295,3 +551,64 @@ void LowerCase(char *string){
 		}
 	}	
 }
+
+
+/* While para verificar de hora em hora apenas
+	while(fgetc(FilePointer)!= EOF){
+			IndexNegative = 0;//contradiz a soma de hora superior para verificar horarios anteriores ao atual
+			//printf("\nComecando em %d\n", IndexPosition);
+			//Procura Hora
+			for(Index=0;fgetc(FilePointer) != ':'; Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				InfoCompare[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			//Move uma posicao depois de : na string
+            IndexPosition++;
+            
+            InfoCompare[2] = '\0';
+            //Adiciona hora a IndexCompare
+            IndexCompare = atoi(InfoCompare);
+            printf("\nHora Atual:%s\n", InfoCompare);
+            //Procura Minutos
+           	for(Index=0;fgetc(FilePointer) != ';'; Index++){
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+				InfoCompare[Index] = fgetc(FilePointer);
+				IndexPosition++;
+			}
+			
+			InfoCompare[2] = '\0';
+			
+			
+
+			//Compara as horas entre si, e caso o horario inserido seja menor que o marcado, e tenha minutos, insere uma hora a mais.
+			Index=atoi(InfoAtual->hour);
+			if(atoi(InfoAtual->minutes)>0 && atoi(InfoAtual->hour)<IndexCompare){
+				Index++;
+			}
+			//Compara minutos, se houver qualquer minuto adicional, conta a hora. Incrementa IndexNegative para contrariar a condicao no caso de horarios anteriores.
+			if(atoi(InfoCompare)>1){
+				IndexCompare++;
+				IndexNegative++;
+			}
+
+			
+
+			if((IndexCompare-IndexNegative)-Index<=1 && IndexCompare-Index >=-1){
+        		printf("Hora ja marcada, pois %d menos %d, resulta em %d", IndexCompare, Index, IndexCompare-Index);
+        		
+			}
+			else
+       			printf("Hora nao marcada, pois %d menos %d, resulta em %d", IndexCompare, Index, IndexCompare-Index);
+		
+
+			//Procura a proxima linha
+			for(;fgetc(FilePointer)!= '\n';){
+				IndexPosition++;
+				//printf("\nAchamos a quebra de linha\n");
+				fseek(FilePointer,IndexPosition*sizeof(char), SEEK_SET);
+			}
+			IndexPosition++;
+
+	
+        }*/
